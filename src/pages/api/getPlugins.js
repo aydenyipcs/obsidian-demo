00b99eb20429f6fcdf50a9081b5324c9b73ecbd5
aydenyipcs <aydenyip56@gmail.com>
2024-05-25 18:07:31 -0400

    debug prod

--- src/pages/api/getPlugins.js
+++ src/pages/api/getPlugins.js
@@ -1,8 +1,11 @@
 const fs = require('fs')
 
 export default async function handler(req, res) {
+   console.log('path hit')
    const jsonDirectory = path.join(process.cwd(), 'public');
-    const fileContents = await fs.readFile(jsonDirectory + '/plugins.json', 'utf8');
+    const filePath = path.join(jsonDirectory, 'plugins.json');
+    const fileContents = await fs.readFile(filePath, 'utf8');
+    console.log('got data')
     const data = JSON.parse(fileContents);
-   res.status(200).json(data)
+    res.status(200).json(data);
 }
