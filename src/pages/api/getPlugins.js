7bdbd5f08cb1ed26df369addcc727d4c4ab76ca1
aydenyipcs <aydenyip56@gmail.com>
2024-05-25 18:09:47 -0400

    imports for prod

--- src/pages/api/getPlugins.js
+++ src/pages/api/getPlugins.js
@@ -1,11 +1,17 @@
-const fs = require('fs')
+import path from 'path';
+import { promises as fs } from 'fs';
 
 export default async function handler(req, res) {
-   console.log('path hit')
-   const jsonDirectory = path.join(process.cwd(), 'public');
-    const filePath = path.join(jsonDirectory, 'plugins.json');
-    const fileContents = await fs.readFile(filePath, 'utf8');
-    console.log('got data')
-    const data = JSON.parse(fileContents);
-    res.status(200).json(data);
+   try {
+      console.log('path hit')
+      const jsonDirectory = path.join(process.cwd(), 'public');
+      const filePath = path.join(jsonDirectory, 'plugins.json');
+      const fileContents = await fs.readFile(filePath, 'utf8');
+      console.log('got data')
+      const data = JSON.parse(fileContents);
+      res.status(200).json(data);
+    } catch (error) {
+      console.error("Error reading plugins file:", error.message);
+      res.status(500).json({ error: 'Failed to read plugins file', details: error.message });
+    }
 }
